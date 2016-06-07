cedf946f6ed4b995eaa4436d33c9992fd242905c
Spider <spider@DesktopWalletMaker.(none)>
2016-06-07 10:10:55 -0700

    changing front end - changing to cpp

---
+++ vangen.h
@@ -0,0 +1,35 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+
+
+
+
+
+/*
+int vg_exec_context_init(_vg_context_s*, _vg_exec_context_s*);
+void vg_exec_context_upgrade_lock(_vg_exec_context_s*);
+void vg_exec_context_downgrade_lock(_vg_exec_context_s*);
+void vg_output_timing(_vg_context_s*, int, timeval*);
+void vg_exec_context_yield(_vg_exec_context_s*);
+void vg_exec_context_del(_vg_exec_context_s*);
+void vg_context_thread_exit(_vg_context_s*);
+
+void vg_regex_context_new(int, int);
+void vg_prefix_context_new(int, int, int);
+void vg_output_match_console(_vg_context_s*, ec_key_st*, char const*);
+void vg_output_timing_console(_vg_context_s*, double, unsigned long long, unsigned long long);
+void vg_context_add_patterns(_vg_context_s*, char const**, int);
+void vg_read_file(_IO_FILE*, char***, int*);
+void vg_prefix_context_set_case_insensitive(_vg_context_s*, int);
+void vg_context_add_patterns(_vg_context_s*, char const**, int);
+void vg_read_password(char*, unsigned int);
+void vg_check_password_complexity(char const*, int);
+*/
